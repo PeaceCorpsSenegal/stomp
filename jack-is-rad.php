8693a77a3e155418a0cc12d03dd3b6480f08d59a
root <root@root.(none)>
2012-05-01 12:27:34 -0400

    initialized plugin file

---
+++ jack-is-rad.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Jack Is Rad
+Plugin URI: http://www.brownjohnf.com
+Description: A Wordpress wrapper for the jQuery Timeline Slider.
+Version: 0.1
+Author: John F. Brown (jQuery Timeline Slider by Pez Flash)
+Author URI:
+License: MIT
+*/
+?>
