0cd57bc14c772ee6e71b70fbef1721fcb20fafcd
root <root@root.(none)>
2012-05-01 13:29:33 -0400

    added comments, version bump

--- wp-jsocial/init.php
+++ wp-jsocial/init.php
@@ -3,12 +3,13 @@
 Plugin Name: wp-jsocial
 Plugin URI: http://www.brownjohnf.com
 Description: A Wordpress wrapper for the jQuery Timeline Slider.
-Version: 0.1
+Version: 0.1.1
 Author: John F. Brown (jQuery Timeline Slider by Pez Flash)
 Author URI: http://unpezvivo.com
 License: MIT
 */
 
+// style name, path, dependencies, version
 wp_enqueue_style(
   "style",
   WP_PLUGIN_URL."/Script/style.css",
@@ -16,6 +17,7 @@ wp_enqueue_style(
   "1.0"
 );
 
+// script name, path, dependencies, version, placement(0=header, 1=footer)
 wp_enqueue_script(
   'jsocial',
   WP_PLUGIN_URL.'/Script/jsocial.js',
@@ -23,6 +25,5 @@ wp_enqueue_script(
   '1.0',
   1
 );
-
 
 ?>
